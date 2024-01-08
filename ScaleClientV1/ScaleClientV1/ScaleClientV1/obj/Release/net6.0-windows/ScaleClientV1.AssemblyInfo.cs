c89c3737746ece16d04ae32f7da85e401d3d90eb
Noregret <noregret3277@gmail.com>
2024-01-08 16:25:19 +0800

    11

--- ScaleClientV1/ScaleClientV1/ScaleClientV1/obj/Release/net6.0-windows/ScaleClientV1.AssemblyInfo.cs
+++ ScaleClientV1/ScaleClientV1/ScaleClientV1/obj/Release/net6.0-windows/ScaleClientV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScaleClientV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cd5e78124e3bc7ddca0d3eae1f1fdecd245009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScaleClientV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScaleClientV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
